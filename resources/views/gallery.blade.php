a1132b998f695c80982ea2be2f6dc067be68037c
Ero Aguilar <eroaguilar7@gmail.com>
2022-03-22 07:34:04 +0800

    Added photos to the gallery.

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -6,9 +6,8 @@
 <section style="background-image: url('{{ asset('images/gallery/gallery-bg.webp') }}'); background-repeat: repeat;">
     <div class="photo-gallery" style="background-color: rgba(255,255,255,0);">
         <div class="container">
-            <img src="{{ asset('images/gallery/gallery.webp') }}" class="img-fluid rounded mt-3"><br>
             <div class="row photos pt-3">
-                @for ($x = 78; $x >= 1; $x--)
+                @for ($x = 97; $x >= 1; $x--)
                 <div class="col-sm-3 col-md-4 col-lg-3 text-center item"><a data-lightbox="photos" href="{{ asset('images/gallery/'.$x.'.webp') }}"><img class="img-fluid border rounded border-white shadow gallery_image" src="{{ asset('images/gallery/'.$x.'.webp') }}" data-aos="fade-up"></a></div>
                 @endfor
             </div>
